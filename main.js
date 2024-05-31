2b05a9ef42ce1a3c4d3145f581e0c6605721b59e
yaroslawa <90762006+yaroslawa@users.noreply.github.com>
2024-05-31 15:54:00 +0300

    Create main.js
    код для кнопки с qr кодом

---
+++ main.js
@@ -0,0 +1,5 @@
+// кнопка на QR на странице index
+const botBtn = document.querySelector('.botBtn');
+botBtn.addEventListener('click', function(){
+    alert('В данный момент телеграм-бот находится на стадии разработки.');
+})
